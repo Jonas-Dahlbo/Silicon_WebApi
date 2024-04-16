d9cbdad824e02838dfd11549f428b5d91114bc25
Jonas-Dahlbo <Jonas.dahlbo@gmail.com>
2024-04-16 22:51:11 +0200

    Added Post, GetOne, GetAll, Patch, Delete

--- WebApi/Controllers/CoursesController.cs
+++ WebApi/Controllers/CoursesController.cs
@@ -1,9 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Infrastructure.Filters;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
+[UseApiKey]
 public class CoursesController : ControllerBase
 {
+    [HttpPost]
+    public async Task<IActionResult> CreateCourse()
+    {
+        return Ok();
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetOneCourse()
+    {
+        return Ok();
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAllCourses()
+    {
+        return Ok();
+    }
+
+    [HttpPatch]
+    public async Task<IActionResult> UpdateCoruse()
+    {
+        return Ok();
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteCourse()
+    {
+        return Ok();
+    }
 }
