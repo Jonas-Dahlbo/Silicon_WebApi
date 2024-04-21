29e0ed5f0ab0c9d98e00c97245427a67c44ea1f2
Jonas-Dahlbo <Jonas.dahlbo@gmail.com>
2024-04-22 00:46:53 +0200

    Removed parts to fix unresolved issue

--- WebApi/Controllers/CoursesController.cs
+++ WebApi/Controllers/CoursesController.cs
@@ -8,33 +8,14 @@ namespace WebApi.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 [UseApiKey]
+
 public class CoursesController(ApiContext context) : ControllerBase
 {
 
     private readonly ApiContext _context = context;
 
-    [HttpPost]
-    public async Task<IActionResult> CreateCourse()
-    {
-        return Ok();
-    }
     
-    [HttpGet]
-    public async Task<IActionResult> GetOneCourse(int Id)
-    {
-        if (ModelState.IsValid)
-        {
-            var Course = await _context.Courses.FirstOrDefaultAsync(x => x.Id == Id);
-            
-            if (Course != null) {
-                return Ok(Course);
-            }
 
-            return NotFound();
-        }
-        
-        return BadRequest();
-    }
     
     [HttpGet]
     public async Task<IActionResult> GetAllCourses()
@@ -47,12 +28,6 @@ public class CoursesController(ApiContext context) : ControllerBase
         return BadRequest();
     }
 
-
-    [HttpPatch]
-    public async Task<IActionResult> UpdateCoruse()
-    {
-        return Ok();
-    }
 
     [HttpDelete]
     public async Task<IActionResult> DeleteCourse(int Id)
